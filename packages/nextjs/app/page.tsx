08702b3f79ae0c19f45bb18aaa586d8dd4e1a14b
Bobby Liu <bubby.liu0420@gmail.com>
2025-06-29 12:42:54 -0500

    Initial changes to the main page to create a PR and continue working on it

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -19,6 +17,7 @@ const Home: NextPage = () => {
             <span className="block text-4xl font-bold">Batch 17</span>
           </h1>
           <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
+          <p>6.11.2025 ~ 6.28.2025</p>
           <p className="text-lg flex gap-2 justify-center">
             <span className="font-bold">Checked in builders count:</span>
             {isLoading ? (
@@ -30,28 +29,7 @@ const Home: NextPage = () => {
         </div>
 
         <div className="grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+          <div className="flex justify-center items-center gap-12 flex-col md:flex-row"></div>
         </div>
       </div>
     </>
